a1b38e5210730a0d2244273f033425f1af6ae858
cstieg <cstieg4899@yahoo.com>
2017-09-16 19:48:28 -0400

    Initial: Create ASP.NET MVC app

---
+++ ProfanityEdit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProfanityEdit.MvcApplication" Language="C#" %>
---
+++ ProfanityEdit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProfanityEdit.Startup))]
+namespace ProfanityEdit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
