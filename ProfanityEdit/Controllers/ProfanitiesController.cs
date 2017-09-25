e761bb808df88c76d1d103787b7ea2c769ea5a78
cstieg <cstieg4899@yahoo.com>
2017-09-25 16:24:37 -0400

    Add: Bind Ask to profanities controllers

--- ProfanityEdit/Controllers/ProfanitiesController.cs
+++ ProfanityEdit/Controllers/ProfanitiesController.cs
@@ -48,7 +48,7 @@ namespace ProfanityEdit.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Word,CategoryId,Level")] Profanity profanity)
+        public ActionResult Create([Bind(Include = "Id,Word,CategoryId,Level,Ask")] Profanity profanity)
         {
             if (ModelState.IsValid)
             {
@@ -82,7 +82,7 @@ namespace ProfanityEdit.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Word,CategoryId,Level")] Profanity profanity)
+        public ActionResult Edit([Bind(Include = "Id,Word,CategoryId,Level,Ask")] Profanity profanity)
         {
             if (ModelState.IsValid)
             {
