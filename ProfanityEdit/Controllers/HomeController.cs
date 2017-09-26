0897dabe3c81828691130c238d47767f4fad6fd1
cstieg <cstieg4899@yahoo.com>
2017-09-26 10:58:06 -0400

    Add: Search for movie

--- ProfanityEdit/Controllers/HomeController.cs
+++ ProfanityEdit/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using ProfanityEdit.Models;
+
 
 namespace ProfanityEdit.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult SearchResults()
+        {
+            var searchTerm = Request.Params.Get("movieName");
+            var searchResults = db.Movies.Include(m => m.EditLists)
+                .Where(m => m.Name.ToLower().Contains(searchTerm.ToLower())).ToList();
+
+            return View(searchResults);
         }
 
         public ActionResult About()
